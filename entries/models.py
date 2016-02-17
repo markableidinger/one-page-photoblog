8aabccddf974bb6e0986dc84773da76a0c05c6bf
Mark Ableidinger <mark.ableidinger@gmail.com>
2016-02-17 15:42:12 -0800

    creates the PhotoBlogEntry object in the entries app

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,14 @@
+"""Models used by Photoblog entries."""
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class PhotoBlogEntry(models.Model):
+    """The model for a blog entry."""
+
+    title = models.CharField(max_length=200, verbose_name='Title')
+    post_body = models.TextField(verbose_name='Post Body')
+    metadata = JSONField(default=dict)
+    photo_file = models.FileField()
+    date = models.DateField()
+    location_name = models.CharField(max_length=100)
