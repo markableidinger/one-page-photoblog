b366bd2db4bfb498085bbb53659b740e2ba4051e
Mark Ableidinger <mark.ableidinger@gmail.com>
2016-02-17 19:47:39 -0800

    basic admin site

--- entries/admin.py
+++ entries/admin.py
@@ -1,3 +1,11 @@
+"""Admin for entries app."""
 from django.contrib import admin
+from .models import PhotoBlogEntry
 
-# Register your models here.
+
+class EntryAdmin(admin.ModelAdmin):
+    """Admin for PhotoBlogEntry object. Currently just the default."""
+
+    pass
+
+admin.site.register(PhotoBlogEntry, EntryAdmin)
